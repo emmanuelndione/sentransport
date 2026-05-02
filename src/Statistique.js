a99f5d044205d46a0660266d60838ae7bafc92ee
Emmanuel Dibor Ndione <emmanueldibor.ndione@email.com>
2026-05-02 18:49:46 +0000

    Lab 2 : composants statiques LigneBus et ListeLignes

---
+++ src/Statistique.js
@@ -0,0 +1,10 @@
+function Statistique({ chiffre, libelle }) {
+  return (
+    <div className="statistique">
+      <h2>{chiffre}</h2>
+      <p>{libelle}</p>
+    </div>
+  );
+}
+
+export default Statistique;
